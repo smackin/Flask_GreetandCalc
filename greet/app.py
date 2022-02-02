6a882614907458ebea35fafa64ae94007e1f1dff
Stephanie <smmackin12@gmail.com>
2022-02-02 16:37:27 -0500

    First Commit

---
+++ greet/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/welcome')
+def welcome_page():
+    """Displays Welcome Page message"""
+    return "Welcome to the Welcome Page."
+
+
+@app.route('/welcome/home')
+def home_page():
+    """Displays Home Page"""
+    return " Welcome to the Home Page."
+
+@app.route('/welcome/back')
+def welcome_back_page():
+    """Page Welcomes returning users"""
+    return "Welcome back!  We've Missed you! "
+    
+
+
